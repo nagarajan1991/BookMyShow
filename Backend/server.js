0829319004be7ff8c2ac8d31a283b89c0b66383a
nagarajan1991 <nagarajan1991@gmail.com>
2025-01-27 00:06:29 +0000

    Part2 27Jan2025

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const connectDB = require("./config/db");
 require("dotenv").config();
-
 const userRoute = require("./routes/userRoute");
+
 connectDB();
 
+app.use(cors());
 app.use(express.json());
 app.use("/bms/users", userRoute);
-app.listen(process.env.PORT, () => {
-  console.log(`server is running on ${process.env.PORT}`);
-});
+
+// Start the server
+const PORT = process.env.PORT || 8000; 
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
