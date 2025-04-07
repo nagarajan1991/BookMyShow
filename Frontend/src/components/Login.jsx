051ee8a75648f842d639ec88798a29ff72504c3d
nagarajan1991 <nagarajan1991@gmail.com>
2025-04-08 00:59:16 +0100

    Part 3

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,54 +1,88 @@
-import React from 'react';
-import {Form, Input, Button} from "antd";
-import { Link } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { Form, Input, Button, message } from "antd";
+import { Link, useNavigate } from "react-router-dom";
+import { LoginUser } from "../api/user";
+import { useDispatch } from "react-redux";
+import { hideLoading, showLoading } from "../redux/loaderSlice";
 
 const Login = () => {
-    return (
-        <header className="App-header">
-            <main className="main-area mw-500 text-center px-3">
-                <section>
-                    <h1>Login to BookMyShow</h1>
-                </section>
-                <section>
-                    <Form layout="vertical">
-    
-                        <Form.Item 
-                        label="Email" 
-                        htmlFor="email" 
-                        name="email" 
-                        className="d-block"
-                        rules={[{required: true, message:"Email is required"}]}>
-                        <Input id="email" type="text" placeholder="Enter your email"></Input>
-                        </Form.Item>
-    
-                        <Form.Item 
-                        label="Password" 
-                        htmlFor="password" 
-                        name="password" 
-                        className="d-block"
-                        rules={[{required: true, message:"Password is required"}]}>
-                        <Input id="email" type="password" placeholder="Enter your password"></Input>
-                        </Form.Item>
-
-                        <Form.Item>
-                        <Button type="primary"
-                        block
-                        htmlfor="login"
-                        style={{fontSize: "1rem", fontWeight:"600"}}>Login</Button>
-                        </Form.Item>
-    
-                    </Form>
-                </section>
-    
-                <section>
-                    <p>New user? 
-                        <Link to="/register"> Register here</Link>
-                    </p>
-                </section>
-    
-            </main>
-        </header>
-      )
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const onFinish = async (values) => {
+    try {
+      dispatch(showLoading());
+      const response = await LoginUser(values);
+      if (response?.success) {
+        message.success(response?.message);
+        localStorage.setItem("tokenForBMS", response?.data);
+        navigate("/");
+      }
+    } catch (error) {
+      message.error(error);
+    } finally {
+      dispatch(hideLoading());
     }
+  };
 
-export default Login
+  useEffect(() => {
+    if (localStorage.getItem("tokenForBMS")) {
+      navigate("/", { replace: true });
+    }
+  }, []);
+  return (
+    <header className="App-header">
+      <main className="main-area mw-500 text-center px-3">
+        <section>
+          <h1>Login to BookMyShow</h1>
+        </section>
+        <section>
+          <Form layout="vertical" onFinish={onFinish}>
+            <Form.Item
+              label="Email"
+              htmlFor="email"
+              name="email"
+              className="d-block"
+              rules={[{ required: true, message: "Email is Required" }]}
+            >
+              <Input
+                id="email"
+                type="email"
+                placeholder="Enter your Email"
+              ></Input>
+            </Form.Item>
+            <Form.Item
+              label="Password"
+              htmlFor="password"
+              name="password"
+              className="d-block"
+              rules={[{ required: true, message: "Password is Required" }]}
+            >
+              <Input
+                id="password"
+                type="password"
+                placeholder="Enter your Password"
+              ></Input>
+            </Form.Item>
+            <Form.Item>
+              <Button
+                type="primary"
+                block
+                htmlType="submit"
+                style={{ fontSize: "1rem", fontWeight: "600" }}
+              >
+                Login
+              </Button>
+            </Form.Item>
+          </Form>
+        </section>
+        <section>
+          <p>
+            New User ? <Link to="/register">Register Here</Link>
+          </p>
+        </section>
+      </main>
+    </header>
+  );
+};
+
+export default Login;
