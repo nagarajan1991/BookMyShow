e97ce783fa7ab7ef6660b0e2a09e643d179a4424
nagarajan1991 <nagarajan1991@gmail.com>
2025-04-09 01:10:14 +0100

    Part4 - Delete movie is not working

--- Backend/server.js
+++ Backend/server.js
@@ -4,15 +4,15 @@ const cors = require("cors");
 const connectDB = require("./config/db");
 require("dotenv").config();
 const userRoute = require("./routes/userRoute");
+const movieRoute = require("./routes/movieRoute");
 
 connectDB();
 
 app.use(cors());
 app.use(express.json());
 app.use("/bms/users", userRoute);
+app.use("/bms/movies", movieRoute);
 
-// Start the server
-const PORT = process.env.PORT || 8000; 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-  });
+app.listen(process.env.PORT, () => {
+  console.log(`server is running on ${process.env.PORT}`);
+});
---
+++ Frontend/src/pages/Admin/TheatreTable.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+const TheatreTable = () => {
+};
+ 
+export default TheatreTable;
